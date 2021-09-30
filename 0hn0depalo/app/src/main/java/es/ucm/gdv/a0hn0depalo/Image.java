a6015fb801c83affe9057fedf67fe9473505dec5
Daniel Cortijo <dcortijo@ucm.es>
2021-09-30 19:11:22 +0200

    Casi todas las interfaces iniciales

---
+++ 0hn0depalo/app/src/main/java/es/ucm/gdv/a0hn0depalo/Image.java
@@ -0,0 +1,10 @@
+package es.ucm.gdv.a0hn0depalo;
+
+public interface Image {
+    String _name = null;
+    float _width = 0, _height = 0;
+
+    String get_name();
+    float get_width();
+    float get_height();
+}
---
+++ 0hn0depalo/app/src/main/java/es/ucm/gdv/a0hn0depalo/Input.java
@@ -0,0 +1,9 @@
+package es.ucm.gdv.a0hn0depalo;
+
+import java.util.List;
+
+public interface Input {
+    List<TouchEvents> getTouchEvents();
+}
+
+class TouchEvents{};
