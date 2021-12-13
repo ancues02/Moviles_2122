63e6521527f3a58467406e81a9bac3a5e16ed3f0
PabloRBGM <prodra02@ucm.es>
2021-12-13 11:50:55 +0100

    Escena de cambio de niveles con scroll
    - Falta scroll en la escena de menu
    - Falta cambiar el tamaño de los displays segun el nº de cosas que se instancien

--- Flow_Free_DelPalo/Assets/src/Managers/LevelSelectorManager.cs
+++ Flow_Free_DelPalo/Assets/src/Managers/LevelSelectorManager.cs
@@ -14,11 +14,23 @@ namespace FlowFree
         public GameObject levelPagePref;
 
         ///<summary>
+        /// Prefab del maracador de la pagina de niveles
+        ///</summary>
+        public GameObject pageMarkerPref;
+
+        ///<summary>
         /// Objeto del canvas que contendra los displays de las paginas
         ///</summary>
         public Transform display;
 
+        ///<summary>
+        /// Objeto del canvas que contendra los marcadores de las paginas
+        ///</summary>
+        public Transform markerDisplay;
+
         public Text packText;
+
+        Image[] markerImages;
 
         ///<summary>
         /// Establece las paginas segun el lote correspondiente
@@ -26,12 +38,27 @@ namespace FlowFree
         public void setPack(LevelPack pack)
         {
             packText.text = pack.packName;
+            markerImages = new Image[pack.pages.Length];
+
             PackDisplay packDplay;
             for (int i = 0; i < pack.pages.Length; i++)
             {
                 packDplay = Instantiate(levelPagePref, display).GetComponent<PackDisplay>();
                 packDplay.setAttributes(pack, i);
+                markerImages[i] = Instantiate(pageMarkerPref, markerDisplay).GetComponent<Image>();
             }
+            markerImages[0].color = Color.white;
+        }
+
+        public void changePageMarker(Vector2 v)
+        {
+            foreach(Image i in markerImages)
+            {
+                i.color = Color.gray;
+            }
+
+            float x = Mathf.Clamp(v.x, 0f, 0.8f);
+            markerImages[(int)(x * markerImages.Length)].color = Color.white;
         }
 
     }
